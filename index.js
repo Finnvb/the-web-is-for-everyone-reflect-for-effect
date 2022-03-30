0134b431b8e4c8b13ba70f1cd80ce7a0d27360d9
Finnvb <finnvb@gmail.com>
2022-03-30 13:51:40 +0200

    Server side rendering setup

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
-const express = require('express');
+const express = require('express')
+const app = express()
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
+// Serve public files
+app.use(express.static('public'))
 
-// const { readFile } = require('fs');
+// Hook up a template engine
+app.set('view engine', 'ejs')
+app.set('views', './views')
 
-const app = express();
-const {readFile} = require('fs').promises;
-
-app.get('/', async(request, response) => {
-
-    response.send(await readFile('./vragenlijst.html', 'utf-8'));
+app.get('/', (req, res) => {
+  // res.send('Hallo wereld!')
+  res.render('index', {
+    title: 'Dit is een index uit EJS',
+  })
 })
 
-// app.get('/', (request, response) => {
+app.get('/flappie', (req, res) => {
+  // res.send('Hallo wereld!')
+  res.render('flappie', {
+    title: 'Dit is flappie',
+  })
+})
 
-//     readFile('./index.html', 'utf-8', (err, html) => {
-//         if(err){
-//             response.status(500).send('Sorry, server error');
-//         }
+app.get('/quotes', (req, res) => {
+  fetchJson('https://quote.api.fdnd.nl/v1/quote').then(function (jsonData) {
+    res.render('quotes', {
+      title: 'Dit is de quotes pagina',
+      quotes: jsonData.data,
+    })
+  })
+})
 
-//         response.send(html);
-//     })
-// })
+app.get('/quote/:quoteId', (request, response) => {
+  fetchJson(`https://quote.api.fdnd.nl/v1/quote/${request.params.quoteId}`).then(function (
+    jsonData
+  ) {
+    response.render('quote', {
+      title: 'Dit is een enkele quote',
+      quote: jsonData.data[0],
+    })
+  })
+})
 
-app.use(express.static('styles'));
+app.set('port', process.env.PORT || 8000)
 
-app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'));
+const server = app.listen(app.get('port'), () => {
+  console.log(`Application started on port: ${app.get('port')}`)
+  console.log('http://localhost:8000');
+})
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
